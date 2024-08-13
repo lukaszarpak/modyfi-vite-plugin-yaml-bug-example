0b97f151cf03253122702fca2ba25f602befd141
Łukasz Szarpak <lukasz.szarpak@gowork.pl>
2024-08-13 11:49:07 +0200

    feat: add @modyfi/vite-plugin-yaml package

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import translations from '../translations/test.en.yml'
+
+console.log('translations', translations)
+
 const app = createApp(App)
 
 app.use(createPinia())
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
+import {resolve} from "node:path";
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ViteYaml from "@modyfi/vite-plugin-yaml";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    ViteYaml(),
+    VueI18nPlugin({
+      include: [resolve(__dirname, "./translations/**")], // this line breaks the app
+    }),
   ],
   resolve: {
     alias: {
